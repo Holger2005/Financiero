bdd74b7e855422d2bae8940356b1f5279f9cd2b9
Holger2005 <ordonezholger43@gmail.com>
2026-09-04 00:58:50 -0500

    refactor: agregar atributo propietario a Cuenta y modificar método imprimir

--- src/com/krakedev/financiero/entidades/Cuenta.java
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -5,12 +5,21 @@ public class Cuenta {
 	private String id;
 	private double saldoActual;
 	private String tipo;
+	private Cliente propetario;
 
 	public Cuenta(String id) {
 
 		this.id = id;
 		this.saldoActual = 0;
 		this.tipo = "A";
+	}
+	
+	public Cliente getPropetario() {
+		return propetario;
+	}
+
+	public void setPropetario(Cliente propetario) {
+		this.propetario = propetario;
 	}
 
 	public String getId() {
@@ -39,7 +48,7 @@ public class Cuenta {
 
 	public void imprimir() {
 		String mensaje;
-		mensaje = "ID: " + id + "Saldo Actual: " + saldoActual + "Tipo: " + tipo;
+		mensaje = "ID: " + id + "Saldo Actual: " + saldoActual + "Tipo: " + tipo + "Cédula cliente: " + propetario.getCedula();
 
 		System.out.println(mensaje);
 	}
